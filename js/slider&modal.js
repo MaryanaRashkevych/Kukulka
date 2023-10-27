4a42b36a06b3ef72a3de1961031f11aa0144b8d9
MaryanaRashkevych <149114863+MaryanaRashkevych@users.noreply.github.com>
2023-10-27 23:17:49 +0300

    First eddition Cities

---
+++ js/slider&modal.js
@@ -0,0 +1,29 @@
+
+let slideToShow = 0;
+
+const modalBtns = document.querySelectorAll('.gallery-modal-btn')
+
+modalBtns.forEach(function (button) {
+    button.addEventListener('click', function () {
+        const slideNumberToShow = button.getAttribute('data-index');
+
+        slideToShow = Number(slideNumberToShow);
+
+        console.log('slideToShow');
+    })
+
+    
+})
+
+//Запуск бібліотеки модального вікна
+MicroModal.init({
+    onShow: function () {
+        slider.go(slideToShow);
+    }
+});
+
+//Запуск біліотеки слайдеру
+
+const sliderContainer = document.querySelector('.splide');
+
+const slider = new Splide(sliderContainer).mount();
